30e290a63b6d566d7e55614a5f0af08fdd63db82
thomaskalnik <kalnik.thomas@gmail.com>
2019-02-05 22:16:33 -0600

    Flask REST app setup and new branch

--- src/MongoREST.py
+++ src/MongoREST.py
@@ -1,8 +1,9 @@
+# importing the Mongo_CRUD file is not working when running this script from the command line
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
+# Use jsonify to return {hello:"world"}
 @app.route("/", methods=['GET'])
 def hello():
     return "hello world"
